4fe5e750d669a08ee5d4854d9a72a9ac6ffb6ed8
SeanAllanMacKay <seanallanmacKay@gmail.com>
2020-07-31 15:15:57 -0500

    Update configs

--- server/index.js
+++ server/index.js
@@ -4,9 +4,6 @@ import { Server } from 'http';
 import path from 'path';
 import bodyParser from 'body-parser';
 
-// eslint-disable-next-line no-underscore-dangle
-const __dirname = path.dirname(new URL(import.meta.url).pathname);
-
 const app = express();
 
 const server = Server(app);
@@ -14,10 +11,10 @@ const server = Server(app);
 app.use(bodyParser.json());
 app.use(express.json());
 
-app.use(express.static(path.join(__dirname, '..', 'build')));
+app.use(express.static('dist'));
 
 app.get('/', (req, res) => {
-  res.sendFile(path.resolve(__dirname.substring(2)));
+  res.sendFile(path.resolve('dist', 'index.html'));
 });
 
 server.listen(process.env.PORT || 8080);
--- webpack.config.js
+++ webpack.config.js
@@ -1,21 +1,49 @@
 const HtmlWebPackPlugin = require('html-webpack-plugin');
+const nodeExternals = require('webpack-node-externals');
 const path = require('path');
 
-module.exports = {
-  context: __dirname,
+const backendConfig = {
+  target: 'node',
+  entry: './server',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'server.js',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js)$/,
+        exclude: /node_modules/,
+        use: 'babel-loader',
+      },
+    ],
+  },
+  externals: [nodeExternals()],
+  resolve: {
+    alias: {
+      '@routes': path.resolve(__dirname, './server/routes'),
+      '@services': path.resolve(__dirname, './server/services'),
+      '@middleware': path.resolve(__dirname, './server/middleware'),
+    },
+  },
+};
+
+const frontendConfig = {
+  target: 'web',
   entry: './src/index.js',
   output: {
     path: path.resolve(__dirname, 'dist'),
     filename: 'main.js',
-    publicPath: '/',
   },
   devServer: {
+    hot: true,
     historyApiFallback: true,
   },
   module: {
     rules: [
       {
-        test: /\.js$/,
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
         use: 'babel-loader',
       },
       {
@@ -40,9 +68,8 @@ module.exports = {
       '@routes': path.resolve(__dirname, './src/routes'),
       '@hooks': path.resolve(__dirname, './src/hooks'),
       '@actions': path.resolve(__dirname, './src/actions'),
-      '@api/routes': path.resolve(__dirname, './server/routes'),
-      '@api/services': path.resolve(__dirname, './server/services'),
-      '@api/middleware': path.resolve(__dirname, './server/middleware'),
     },
   },
 };
+
+module.exports = [backendConfig, frontendConfig];
