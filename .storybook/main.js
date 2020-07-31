cf1103a25bcf27b0ab7a1169100dcc3f8ca0a896
SeanAllanMacKay <seanallanmacKay@gmail.com>
2020-07-31 01:55:25 -0500

    Added storybook

---
+++ .storybook/main.js
@@ -0,0 +1,6 @@
+const path = require('path');
+
+module.exports = {
+  stories: ['../src/components/**/*.story.js'],
+  addons: ['@storybook/addon-knobs', '@storybook/addon-actions'],
+};
---
+++ src/components/Button/Button.story.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+import Button from './index';
+
+export default { title: 'Button' };
+
+export const usage = () => <Button />;
